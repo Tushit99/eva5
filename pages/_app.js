ac3735658c8337466099797c0e3b69ea5193f18b
Tushit99 <97578587+Tushit99@users.noreply.github.com>
2023-01-09 14:25:24 +0530

    done

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import '../styles/globals.css'
+import "../styles/globals.css"; 
+import { ChakraProvider } from "@chakra-ui/react";
+import Navbar from "./Navbar";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider> 
+      <Navbar /> 
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
